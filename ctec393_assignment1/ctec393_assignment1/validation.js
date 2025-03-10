aae76e2a4c4dd12977e4a40dd2c9ab38154b353c
Ulises Servellon <75108825+Ulises-JS@users.noreply.github.com>
2025-03-09 23:36:58 -0400

    Delete ctec393_assignment1/ctec393_assignment1 directory

--- ctec393_assignment1/ctec393_assignment1/validation.js
+++
@@ -1,9 +0,0 @@
-function validateForm() {
-    const name = document.getElementById('name').value;
-    if (name === "") {
-        alert("Please enter your name.");
-        return false;
-    }
-    // Add validation for other fields (email, phone, card info) as needed
-    return true;
-}
